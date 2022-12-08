4ec30d5f7d2632f851f8904c6837e41dd23df1de
henrymutega <hmutega@gmail.com>
2022-12-08 13:47:49 -0800

    Program to print.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/* 
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success.
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }	
