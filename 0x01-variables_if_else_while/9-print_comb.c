a5d34a3154f0e31de261f496606f924ed1974213
henrymutega <hmutega@gmail.com>
2022-12-09 08:21:50 -0800

    My files commit.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,18 @@
 
 int main(void)
 {
-	int n = 48;
-	int com = 44;
-	while (n <= 57)
-	{
-		putchar(n);
-		if (n != 57)
-		{
-			putchar(com);
-			putchar(32);
-		}
-		n += 1;
-	}
-	putchar('\n');
-	return (0);
+int n = 48;
+int com = 44;
+while (n <= 57)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(com);
+putchar(32);
+}
+n += 1;
+}
+putchar('\n');
+return (0);
 }
