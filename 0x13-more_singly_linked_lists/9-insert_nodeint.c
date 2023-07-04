b35e4500a2d5bcfd822d33d015453b7c0ae63a7a
henrymutega <hmutega@gmail.com>
2023-07-04 12:37:27 -0700

    function that deletes the node at index linked list.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * insert_nodeint_at_index - Inserts a new node to a listint_t
  *                           list at a given position.
@@ -7,6 +8,7 @@
  * @idx: The index of the listint_t list where the new
  *       node should be added - indices start at 0.
  * @n: The integer for the new node to contain.
+ *
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new node.
  */
